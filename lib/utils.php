855c7efba63d99192d3052bb78dc25806ede928c
Christian Petersen <humanoidism@gmail.com>
2012-10-29 21:27:06 +0100

    Add more utilities

--- lib/utils.php
+++ lib/utils.php
@@ -24,4 +24,32 @@ class Utils {
 		var_dump($mixed);
 		echo '</pre>';
 	}
+
+	/**
+	 * Creates the directory
+	 * the script die()'s if mkdir() fails
+	 * @param  string $directory
+	 */
+	function createDirectory($directory) {
+		if(!@mkdir(buildPath($directory))) {
+			die("FAILED to create directory\n");
+		}
+	}
+
+	/**
+	 * Create a file for the example with the given $content
+	 * @param  string $directory Directory without / at the end
+	 * @param  string $filename  Filename without path and extension
+	 * @param  string $extension Extension of the file without "."
+	 * @param  string $content   The content of the file
+	 */
+	public static function createFile($directory, $filename, $extension, $content = "") {
+		$handle = @fopen(buildPath($directory, $filename, $extension), "w");
+		if($handle) {
+			fwrite($handle, $content);
+			fclose($handle);
+		} else {
+			die("FAILED to create file\n");
+		}
+	}
 }
